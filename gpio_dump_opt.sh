1b7b7273b298c8c62493d28fed57e074c6432aa4
Karl Palsson <karlp@tweak.net.au>
2014-09-17 01:03:09 +0000

    scripts: try testing for busybox
    And use for loops that work without bash.

--- gpio_dump_opt.sh
+++ gpio_dump_opt.sh
@@ -3,7 +3,11 @@
 # http://www.freaktab.com/showthread.php?7021-GPIO-module-to-check-gpios-in-wifi-and-bt-Lets-make-the-wifi-and-BT-work-in-more-devices&p=124430&viewfull=1#post124430
 # License therefore completely unknown, but presumably copyright "raxy, gormar, leolas"
 
+# Not all machines have useful tools, or the busybox symlinks installed
+# attempt to set a prefix for busybox to get less common tools
 b=""
+[ -f /system/bin/sed ] || b="busybox"
+GPIO_BINARY=/data/karl/gpio
 b_has_dc=`$b dc 1 1 + p 2>/dev/null`
 b_has_expr=`$b expr 1 + 1 2>/dev/null`
 OUT=GPIO-DUMP-
@@ -24,7 +28,7 @@ echo "User GPIO module loaded!"
 echo
 
 
-for conf in {1..7}
+for conf in $($b seq 1 7)
 do
 for on in 0 1
 do
@@ -33,14 +37,14 @@ gpio_nr=159
 end=287
 
 OUTF="${OUT}conf$conf-$([ "$on" == "1" ] && echo freqon || echo alloff).RF"
-for n in {0..2}
+for n in $($b seq 0 2)
 do
   switch=/sys/class/rfkill/rfkill$n
-  OUTF="${OUTF}$(echo $(( ($conf >> $n) & $on )) | tee $switch/state)"
+  OUTF="${OUTF}$(echo $(( ($conf >> $n) & $on )) | $b tee $switch/state)"
 done
 
 OUTF="$OUTF.log"
-for n in {0..2}
+for n in $($b seq 0 2)
 do
   switch=/sys/class/rfkill/rfkill$n
   echo "*** $switch ***" >> $OUTF
@@ -70,14 +74,14 @@ do
   
     gpio_goff=`$b echo -n $gpio_goff | $b sed -e 's/0/A/' -e 's/1/B/' -e 's/2/C/' -e 's/3/D/'`
         $b echo -n "$gpio_num: RK30_PIN$gpio_bank"_"P$gpio_goff$gpio_off = " >> $OUTF
-        gpio get $gpio_num >> $OUTF
+        $GPIO_BINARY get $gpio_num >> $OUTF
 done
 echo $OUTF written
 
 done
 done
 
-for i in {1..7}
+for i in $($b seq 1 7)
 do
-    diff -u0 *conf$i*log > ${OUT}conf$i-unified.diff
+    $b diff -u *conf$i*log > ${OUT}conf$i-unified.diff
 done
